3c3dbe40b1d9e04a0ea1f1c8f3cb415978124310
UmSamu <samuel.silva@sptech.school>
2023-04-13 21:56:26 -0300

    Script com dados mockados

--- script.sql
+++ script.sql
@@ -67,6 +67,8 @@ valorMin int,
 valorMax int,
 espaco float,
 tipoPlanta varchar(20),
+constraint chkTipoPlanta 
+	check (tipoPlanta in ('americana', 'crespa', 'mimosa', 'roxa')),
 fkPlantacao int,
 constraint pkSetorPlantacao primary key (idSetorPlantacao, fkPlantacao)
 );
@@ -156,8 +158,55 @@ insert into plantacao values
 	(null, 1950.0, 5);
     
 insert into setorPlantacao values 
-	(null, 'Setor 1', 300, 900, );
+	(null, 'Setor 1', 300, 900, 2500.5, 'Americana', 1),
+	(null, 'Setor 2', 500, 700, 2000, 'Crespa', 1),
+	(null, 'Setor 1', 450, 1000, 6000, 'Mimosa', 2),
+	(null, 'Setor 2', 200, 800, 4000.5, 'Roxa', 2),
+	(null, 'Setor 1', 300, 900, 2200, 'Americana', 3),
+	(null, 'Setor 2', 500, 700, 1000, 'Crespa', 3),
+	(null, 'Setor 1', 200, 800, 1050.25, 'Roxa', 4),
+	(null, 'Setor 2', 450, 1000, 1050.25, 'Mimosa', 4),
+	(null, 'Setor 1', 300, 900, 1300.5, 'Americana', 5),
+	(null, 'Setor 1', 200, 800, 3900.5, 'Roxa', 6),
+	(null, 'Setor 1', 500, 700, 2050, 'Crespa', 7),
+	(null, 'Setor 2', 450, 1000, 2000, 'Mimosa', 7),
+	(null, 'Setor 1', 300, 900, 1450, 'Americana', 8),
+	(null, 'Setor 2', 500, 700, 500, 'Crespa', 8);
+    
 insert into sensor values 
-	();
+	(null, 'ldr', 'ativo', 1),
+	(null, 'ldr', 'manutencao', 2),
+	(null, 'ldr', 'inativo', 3),
+	(null, 'ldr', 'ativo', 4), 
+	(null, 'ldr', 'ativo', 5),
+	(null, 'ldr', 'ativo', 6),
+	(null, 'ldr', 'inativo', 7),
+	(null, 'ldr', 'ativo', 8),
+	(null, 'ldr', 'ativo', 9),
+	(null, 'ldr', 'manutencao', 10),
+	(null, 'ldr', 'ativo', 11),
+	(null, 'ldr', 'inativo', 12),
+	(null, 'ldr', 'manutencao', 13),
+	(null, 'ldr', 'ativo', 14);
+
 insert into leituraSensor values 
-	();
+	(null, 500, '2023-04-01 00:00:00', 1),
+	(null, 600, '2023-04-02 00:00:00', 1),
+	(null, 700, '2023-04-03 00:00:00', 2),
+	(null, 800, '2023-04-04 00:00:00', 3),
+	(null, 900, '2023-04-05 00:00:00', 4),
+	(null, 500, '2023-04-05 12:00:00', 4),
+	(null, 450, '2023-04-07 00:00:00', 5),
+	(null, 700, '2023-04-08 00:00:00', 6),
+	(null, 350, '2023-04-09 00:00:00', 7),
+	(null, 650, '2023-04-10 15:00:00', 8),
+	(null, 250, '2023-04-11 00:00:00', 9),
+	(null, 750, '2023-04-11 12:00:00', 9),
+	(null, 900, '2023-04-12 00:00:00', 9),
+	(null, 1000, '2023-04-02 00:00:00', 10),
+	(null, 300, '2023-04-03 00:00:00', 10),
+	(null, 800, '2023-04-04 00:00:00', 11),
+	(null, 800, '2023-04-05 12:00:00', 11),
+	(null, 400, '2023-04-06 19:00:00', 12),
+	(null, 900, '2023-04-07 00:00:00', 13),
+	(null, 750, '2023-04-08 00:00:00', 14);
