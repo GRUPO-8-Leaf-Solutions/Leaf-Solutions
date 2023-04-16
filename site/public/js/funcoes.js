3488246014235396b69644e4f01f066bd2759f30
h.Cauã <cauaempr@gmail.com>
2023-04-15 23:00:18 -0300

    inserção da API web-data-viz no cadastro empresa

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -32,3 +32,20 @@ function simuladorFinanceiro() {
             `
     }
 }
+
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+
+function finalizarAguardar(texto) {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    var divErrosLogin = document.getElementById("div_erros_login");
+    if (texto) {
+        divErrosLogin.style.display = "flex";
+        divErrosLogin.innerHTML = texto;
+    }
+}
