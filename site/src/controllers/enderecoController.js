45623a24a4c452ce6539497456b8940839ee4e63
h.Cauã <cauaempr@gmail.com>
2023-04-16 01:03:16 -0300

    processo de cadastro endereco

---
+++ site/src/controllers/enderecoController.js
@@ -0,0 +1,76 @@
+var enderecoModel = require("../models/enderecoModel");
+
+var sessoes = [];
+
+function testar(req, res) {
+    console.log("ENTRAMOS NA enderecoController");
+    res.json("ESTAMOS FUNCIONANDO!");
+}
+
+function listar(req, res) {
+    enderecoModel.listar()
+        .then(function (resultado) {
+            if (resultado.length > 0) {
+                res.status(200).json(resultado);
+            } else {
+                res.status(204).send("Nenhum resultado encontrado!")
+            }
+        }).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao realizar a consulta! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var estado = req.body.estadoServer;
+    var cidade = req.body.cidadeServer;
+    var cep = req.body.cepServer;
+    var numEndereco = req.body.numEnderecoVar;
+    var logradouro = req.body.logradouroVar;
+    var complemento = req.body.complementoServer;
+
+
+
+    // Faça as validações dos valores
+    if (estado == undefined) {
+        res.status(400).send("Seu estado está undefined!");
+    } else if (cidade == undefined) {
+        res.status(400).send("Seu cidade está undefined!");
+    } else if (cep == undefined) {
+        res.status(400).send("Seu cep está undefined!");
+    }else if (numEndereco == undefined) {
+        res.status(400).send("Seu numero de endereço está undefined!");
+    }else if (logradouro == undefined) {
+        res.status(400).send("Seu logradouro está undefined!");
+    }else if (complemento == undefined) {
+        res.status(400).send("Seu complemento está undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        enderecoModel.cadastrar(estado, cidade, cep, numEndereco, logradouro, complemento)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    testar,
+    cadastrar,
+    listar
+}
