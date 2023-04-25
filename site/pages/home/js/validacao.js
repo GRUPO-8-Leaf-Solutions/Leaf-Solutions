5aa1637a3f56d5f92280d4d057fb4edc0bc7744b
h.Cauã <cauaempr@gmail.com>
2023-04-24 21:06:18 -0300

    atualização pagina estatistica

--- site/pages/home/js/validacao.js
+++ site/pages/home/js/validacao.js
@@ -1,8 +1,10 @@
-/* function login() {
+function login() {
     var email = inputEmail.value
     var token = inputToken.value
 
-    if (email == leafAdm@leaf.com && token == 123456) {
+    if (email == "leafAdm@leaf.com" && token == 123456) {
         window.location.href = "../dashboard/dashboardPage.html"
+    }else{
+        alert("Senha e/ou Usuario incorreto")
     }
-} */
+}
