d6ea4079ab77981d8384341c502a85c45aab04e4
h.Cauã <cauaempr@gmail.com>
2023-05-16 22:47:42 -0300

    conexão

---
+++ site/src/models/usuarioModel.js
@@ -0,0 +1,53 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function login(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM empresa WHERE email = '${email}' AND tokenPerm = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, email, cnpj) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, cnpj);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO empresa VALUES (null, '${cnpj}', '${nome}', '${email}', null);
+       `
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrarEnd(cep, logradouro, numEndereco, complemento, estado, cidade) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",estado, logradouro, complemento, numEndereco, cidade, cep);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+
+    // informações de acordo com a tabela endereço do banco de dados  
+    var instrucao = `
+        INSERT INTO endereco (CEP, logradouro, numero, complemento, uf, cidade) VALUES ('${cep}', '${logradouro}', '${complemento}', '${numEndereco}', '${cidade}', '${estado}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    login,
+    cadastrar,
+    cadastrarEnd,
+    listar,
+};
---
+++ site/src/routes/usuarios.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+var usuarioControllerEnd = require("../controllers/usuarioControllerEnd");
+
+router.get("/", function (req, res) {
+    usuarioController.testar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    usuarioController.listar(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+});
+
+router.post("/cadastrarEnd", function (req, res) {
+    usuarioControllerEnd.cadastrarEnd(req, res);
+});
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.entrar(req, res);
+});
+
+module.exports = router;
