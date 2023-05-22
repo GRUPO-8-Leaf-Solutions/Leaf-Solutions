7d8be9e2866390c054b8d1d8a4ece4c592dadd0f
lisandrasc <lisandra.cunha@etec.sp.gov.br>
2023-05-22 15:44:10 -0300

    cadastroEstufa ok

--- site/src/models/usuarioEndModel.js
+++ site/src/models/usuarioEndModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config")
 
 
-function cadastrarEnd(cep, logradouro, numEndereco, complemento, estado, cidade) {
+function cadastrarEnd(cep, logradouro, numEndereco, complemento, estado, cidade, fkEmpresa) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",estado, logradouro, complemento, numEndereco, cidade, cep);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
@@ -9,7 +9,7 @@ function cadastrarEnd(cep, logradouro, numEndereco, complemento, estado, cidade)
 
     // informações de acordo com a tabela endereço do banco de dados  
     var instrucao = `
-        INSERT INTO endereco (CEP, logradouro, numero, complemento, uf, cidade) VALUES ('${cep}', '${logradouro}', '${numEndereco}', '${complemento}', '${estado}', '${cidade}');
+        INSERT INTO endereco (CEP, logradouro, numero, complemento, uf, cidade, fkEmpresa) VALUES ('${cep}', '${logradouro}', '${numEndereco}', '${complemento}', '${estado}', '${cidade}',  '${fkEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
